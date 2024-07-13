9b2f40413495590a58bf41ccc1155a41cec3a195
dy102 <ekagnlek2@gmail.com>
2024-07-13 13:30:51 +0900

    refactor: static import

--- src/main/java/app/feedback/member/AuthenticationValidator.java
+++ src/main/java/app/feedback/member/AuthenticationValidator.java
@@ -1,11 +1,12 @@
 package app.feedback.member;
 
 import app.feedback.auth.dto.Authentication;
-import app.feedback.common.exception.CustomErrorCode;
 import app.feedback.common.exception.CustomException;
-import app.feedback.member.domain.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import static app.feedback.common.exception.CustomErrorCode.FORBIDDEN;
+import static app.feedback.member.domain.Role.ADMIN;
 
 @Component
 @RequiredArgsConstructor
@@ -13,19 +14,19 @@ public class AuthenticationValidator {
 
     public void validateAdmin(final Authentication authentication) {
         if (!isAdmin(authentication)) {
-            throw new CustomException(CustomErrorCode.FORBIDDEN);
+            throw new CustomException(FORBIDDEN);
         }
     }
 
     public void validateAdminOrMe(final Authentication authentication,
                                   final String memberId) {
         if (!isAdmin(authentication) && !isMe(authentication, memberId)) {
-            throw new CustomException(CustomErrorCode.FORBIDDEN);
+            throw new CustomException(FORBIDDEN);
         }
     }
 
     private boolean isAdmin(final Authentication authentication) {
-        return authentication.role().equals(Role.ADMIN);
+        return authentication.role().equals(ADMIN);
     }
 
     private boolean isMe(final Authentication authentication,
